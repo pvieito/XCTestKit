80b0da2883de3c92c54d175e90bd137f3f5c7073
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-05-24 19:59:16 +0200

    [XCTestKit] Added support for copying text

--- XCTestKit/XCUIElement+Cocoa.swift
+++ XCTestKit/XCUIElement+Cocoa.swift
@@ -14,40 +14,75 @@ import XCTest
 extension XCUIElement {
     public func openDocuments(at url: URL, selectDirectoryContents: Bool = false) {
         let url = url.resolvingSymlinksInPath().absoluteURL
-        self.typeKey("o", modifierFlags: .command)
-        self.typeKey("g", modifierFlags: [.command, .shift])
+        self.synthesizeOpenKeyboardShortcut()
+        self.synthesizeGoToKeyboardShortcut()
         self.typeText(url.path)
         self.typeText(XCUIKeyboardKey.enter.rawValue)
         
         if selectDirectoryContents {
             self.typeText(XCUIKeyboardKey.rightArrow.rawValue)
-            self.typeKey("a", modifierFlags: [.command])
+            self.synthesizeSelectAllKeyboardShortcut()
         }
         
         self.typeText(XCUIKeyboardKey.enter.rawValue)
     }
     
     public func saveDocument(to url: URL? = nil, filename: String? = nil, pathExtension: String? = nil, assertDefaultFileName: String? = nil) -> URL {
-        self.typeKey("s", modifierFlags: .command)
+        self.synthesizeSaveKeyboardShortcut()
         self.assertNoErrorAlert()
-        
+
         if let assertDefaultFileName = assertDefaultFileName {
-            self.typeKey("c", modifierFlags: .command)
-            #if os(macOS)
-            XCTAssertEqual(NSPasteboard.general.string(forType: .string), assertDefaultFileName)
-            #else
-            XCTAssertEqual(UIPasteboard.general.string, assertDefaultFileName)
-            #endif
+            let selectedText = self.copySelectedText()
+            XCTAssertEqual(selectedText, assertDefaultFileName)
         }
         
         var outputURL = url ?? FileManager.default.temporaryRandomFileURL(
             filename: filename, pathExtension: pathExtension)
         outputURL = outputURL.resolvingSymlinksInPath().absoluteURL
-        self.typeKey("g", modifierFlags: [.command, .shift])
+        self.synthesizeGoToKeyboardShortcut()
         self.typeText(outputURL.path)
         self.typeKey(XCUIKeyboardKey.enter.rawValue, modifierFlags: [])
         self.typeKey(XCUIKeyboardKey.enter.rawValue, modifierFlags: [])
         return outputURL
+    }
+}
+
+extension XCUIElement {
+    private func synthesizeOpenKeyboardShortcut() {
+        self.typeKey("o", modifierFlags: .command)
+    }
+    
+    private func synthesizeSaveKeyboardShortcut() {
+        self.typeKey("s", modifierFlags: .command)
+    }
+    
+    private func synthesizeSelectAllKeyboardShortcut() {
+        self.typeKey("a", modifierFlags: .command)
+    }
+    
+    private func synthesizeCopyKeyboardShortcut() {
+        self.typeKey("c", modifierFlags: .command)
+    }
+    
+    private func synthesizeGoToKeyboardShortcut() {
+        self.typeKey("g", modifierFlags: [.command, .shift])
+    }
+}
+
+extension XCUIElement {
+    public func copySelectedText() -> String? {
+        self.synthesizeCopyKeyboardShortcut()
+        #if os(macOS)
+        let copiedText = NSPasteboard.general.string(forType: .string)
+        #else
+        let copiedText = UIPasteboard.general.string
+        #endif
+        return copiedText
+    }
+    
+    public func copyAllText() -> String? {
+        self.synthesizeSelectAllKeyboardShortcut()
+        return self.copySelectedText()
     }
 }
 #endif
